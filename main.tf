b5dc5e26df9429b6b33d62b800ecd0978148215a
Yashvi <imyashvi2810@gmail.com>
2025-12-21 01:24:23 +0530

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,23 @@
+﻿terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "eu-north-1"
+}
+
+resource "aws_dynamodb_table" "resume_counter" {
+  name         = "yashvi-terraform-counter"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "id"
+
+  attribute {
+    name = "id"
+    type = "S"
+  }
+}
